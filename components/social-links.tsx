8f0881228aebdd3bd87a8fa1ee9d27bd59a9fce7
Shantam_Shukla <shantamshuklagkp@gmail.com>
2024-10-09 21:15:49 +0530

    Initial commit

---
+++ components/social-links.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const SocialMediaLinks = () => {
+    return (
+        <div className='flex flex-row gap-4'>
+            <a href="https://github.com/ShantamShukla" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faGithub} size="1x" />
+            </a>
+            
+            <a href="https://www.linkedin.com/in/shantam-shukla/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} size="1x" />
+            </a>
+        </div>
+    );
+};
+
+export default SocialMediaLinks;
